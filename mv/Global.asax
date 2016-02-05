b1fb48c8c995dfa8e836638445b0b8d479c9eadf
basaksalli <basaksalli@gmail.com>
2016-02-05 18:57:33 +0200

    o

---
+++ mv/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mv.MvcApplication" Language="C#" %>
---
+++ mv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mv.Startup))]
+namespace mv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
