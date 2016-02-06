4f0ff8eea40377a354f7643f5cc728f1e3c3214a
Cem Özkılıç <cemmozkilic@gmail.com>
2016-02-06 18:44:35 +0200

    jj

--- mv/Controllers/HomeController.cs
+++ mv/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace mv.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
@@ -26,5 +27,7 @@ namespace mv.Controllers
 
             return View();
         }
+
+      
     }
 }
