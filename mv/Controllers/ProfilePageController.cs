67a5c90aa92941fa284c6b5c164942662c43453c
ddmryrk <d.demiryurek92@gmail.com>
2016-02-12 16:44:52 +0200

    asdas

---
+++ mv/Controllers/ProfilePageController.cs
@@ -0,0 +1,33 @@
+﻿using mv.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mv.Controllers
+{
+    public class ProfilePageController : Controller
+    {
+        // GET: ProfilePage
+        Model1 ent = new Model1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Friends()
+        {
+            //ession["UserID"] = 1;
+            var friends = (from f in ent.UserRelationShips
+                           where f.UserID1 ==1 && f.Status == 2
+                           select f.UserID2).ToList();
+            List<Users> friendDetails = new List<Users>();
+            foreach (var item in friends)
+            {
+                friendDetails.Add((from fd in ent.Users where fd.ID == item select fd).FirstOrDefault());
+            }
+            return PartialView(friendDetails);
+        }
+    }
+}
